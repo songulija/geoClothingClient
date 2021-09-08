625fa768105b925d39312b92031124db448e1479
songulija <65647664+songulija@users.noreply.github.com>
2021-09-08 23:24:07 +0300

    Bootstrap added

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Header() {
+    return (
+        <div>
+            <ul>
+                <li>Home</li>
+            </ul>
+        </div>
+    )
+}
+
+export default Header
